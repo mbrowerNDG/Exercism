e6fcf2b331fd0125a8ac5e9b9da2d90c44e37c71
Matthew Brower <Matthew@Matthews-iMac.fios-router.home>
2020-05-21 16:33:10 -0400

    removed unfinished exorcise

--- dart/word-count/lib/word_count.dart
+++ dart/word-count/lib/word_count.dart
@@ -1,23 +1,11 @@
-// class WordCount {
-//   final regex = RegExp(r"(\w+'?\w+|\d+|\w+)");
-//   Map countWords(String phrase) {
-//     var result = Map<String, int>();
-//     regex
-//         .allMatches(phrase.toLowerCase())
-//         .map((match) => match.group(0))
-//         .forEach((word) =>
-//             result.update(word, (int val) => ++val, ifAbsent: () => 1));
-//     return result;
-//   }
-// }
-
 class WordCount {
   final regex = RegExp(r"(\w+'?\w+|\d+|\w+)");
-  Map countWords(String phrase) {
-    splitToList(phrase).fold(
-        (word) => result.update(word, (int val) => ++val, ifAbsent: () => 1));
-    return result;
-  }
+
+  Map countWords(String phrase) =>
+      splitToList(phrase).fold<Map<String, int>>({}, updateCount);
+
+  Map<String, int> updateCount(Map<String, int> result, String word) =>
+      result..update(word, (val) => ++val, ifAbsent: () => 1);
 
   Iterable<String> splitToList(String phrase) =>
       regex.allMatches(phrase.toLowerCase()).map((match) => match.group(0));
